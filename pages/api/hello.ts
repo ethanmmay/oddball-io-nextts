579c7617dcf3bb0065029147ab3cdff070a6ad12
ethanmmay <ethaniel.may@gmail.com>
2023-01-09 20:51:47 -0700

    API Request Experimentation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+interface Quote {
+  quote: string,
+  error?: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Quote>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    res.status(200).json({ quote: 'Get Quote' })
+  } catch (err) {
+    res.status(500).json({ quote: 'NA', error: 'Failed to load Data' })
+  }
 }
