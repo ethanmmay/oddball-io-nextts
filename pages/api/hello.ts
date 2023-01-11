fda91111d0cc45c31bb51b9af2b6383cc91ca69b
ethanmmay <ethaniel.may@gmail.com>
2023-01-10 18:28:56 -0700

    Building Joke Request

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,18 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { InferGetStaticPropsType } from "next/types"
 
-interface Quote {
-  quote: string,
-  error?: string;
-}
+export const getStaticProps = async () => {
+  const response = await fetch('https://backend-omega-seven.vercel.app/api/getjoke');
+  const joke = await response.json();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Quote>
-) {
-  try {
-    res.status(200).json({ quote: 'Get Quote' })
-  } catch (err) {
-    res.status(500).json({ quote: 'NA', error: 'Failed to load Data' })
+  return {
+    props: {
+      joke,
+    },
   }
 }
+
+function DevJoke({ joke }: InferGetStaticPropsType<typeof getStaticProps>) {}
---
+++ pages/api/quote.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface Quote {
+    quote: string,
+    error?: string;
+}
+
+export default async function getQuote(
+    req: NextApiRequest,
+    res: NextApiResponse<Quote>
+) {
+    try {
+        res.status(200).json({ quote: 'Get Quote' })
+    } catch (err) {
+        res.status(500).json({ quote: 'NA', error: 'Failed to load Data' })
+    }
+}
